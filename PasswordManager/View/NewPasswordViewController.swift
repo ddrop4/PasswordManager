b8d245648fb4eca830b0c7076973c8eb486bde54
Anton <antoncherb@gmail.com>
2019-12-18 20:31:57 +0300

    Added save button, added dismiss keyboard method and added navigation titles

--- PasswordManager/View/NewPasswordViewController.swift
+++ PasswordManager/View/NewPasswordViewController.swift
@@ -20,6 +20,9 @@ class NewPasswordViewController: UITableViewController {
         self.tableView.delegate = self
         self.tableView.dataSource = self
         registerCell()
+        setupSaveButton()
+        dismissKeyboard()
+        setupNavigationTitle()
     }
 
     // MARK: - Table view data source
@@ -35,16 +38,18 @@ class NewPasswordViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "passwordCell", for: indexPath)
-        cell.backgroundColor = .white
-        cell.textLabel?.textColor = .lightGray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "passwordCell", for: indexPath) as! PasswordViewCell
+        let passwordField = UITextField(frame: CGRect(x: 20, y: 0, width: cell.frame.size.width, height: cell.frame.size.height))
+        passwordField.placeholder = "Text"
+        
+        cell.addSubview(passwordField)
         
         if indexPath.section == 0 {
-            cell.textLabel?.text = newPasswordArray[indexPath.row]
+            passwordField.placeholder = newPasswordArray[indexPath.row]
         } else if indexPath.section == 1 {
-            cell.textLabel?.text = comment[indexPath.row]
+            passwordField.placeholder = comment[indexPath.row]
         } else {
-            cell.textLabel?.text = tag[indexPath.row]
+            passwordField.placeholder = tag[indexPath.row]
         }
         return cell
     }
@@ -57,9 +62,48 @@ class NewPasswordViewController: UITableViewController {
         return 3
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let firstTitle = "Add your comment"
+        let secondTitle = "Add your tag"
+        if section == 1 {
+            return firstTitle
+        } else if section == 2 {
+            return secondTitle
+        }
+        return title
+    }
+    
     private func registerCell() {
         self.tableView = UITableView(frame: .zero, style: .grouped)
         self.tableView.register(PasswordViewCell.self, forCellReuseIdentifier: "passwordCell")
+    }
+    
+    private func setupSaveButton() {
+        let window = UIWindow()
+        let width = window.frame.size.width / 2 - 50
+        let saveButton = UIButton(type: .custom)
+        let customView = UIView(frame: CGRect(x: Int(width), y: 20, width: 100, height: 45))
+        
+        saveButton.frame = CGRect(x: 0, y: 0, width: 100, height: 35)
+        saveButton.setTitle("Save", for: .normal)
+        saveButton.setTitleColor(.systemBlue, for: .normal)
+        saveButton.addTarget(self, action: #selector(actionSave), for: .touchUpInside)
+        
+        customView.addSubview(saveButton)
+        self.tableView.tableFooterView = customView
+    }
+    
+    @objc private func actionSave() {
+        print(#function)
+    }
+    
+    private func dismissKeyboard() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    private func setupNavigationTitle() {
+        self.navigationItem.title = "Add password"
     }
     
 }
